aed5a9f8769663f9954555e1b1af02eea6fe34b2
Subhash Hardaha <subhashhardaha@gmail.com>
2018-01-14 12:15:28 +0000

    Added files for heroku debug false

--- instaquote/settings.py
+++ instaquote/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
