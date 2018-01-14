6592c53ee204e9d390f4c222ce541ade32117cc6
Subhash Hardaha <subhashhardaha@gmail.com>
2018-01-14 15:30:38 +0000

    Modified View/Changed TimeZone

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,8 +6,18 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text='Inform a valid email address.')
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+    
+        for fieldname in ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            
+            
+print (SignUpForm())
+        
--- instaquote/settings.py
+++ instaquote/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'quotes',
     'accounts',
+    'bootstrap3',ç
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
